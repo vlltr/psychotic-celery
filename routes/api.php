b290b4bdd7c6367ae52b7aed68716f74d640d199
Mauricio Villatoro <mauripr26@gmail.com>
2023-01-21 20:24:18 -0600

    :sparkles: Add Login User

--- routes/api.php
+++ routes/api.php
@@ -4,4 +4,5 @@ use Illuminate\Support\Facades\Route;
 
 Route::prefix('v1')->group(function () {
     Route::post('auth/register', \App\Http\Controllers\Api\V1\Auth\RegisterController::class);
+    Route::post('auth/login', \App\Http\Controllers\Api\V1\Auth\LoginController::class);
 });
