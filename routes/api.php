57f1e8230e1b2a69a498392a93c1c68fb4b93f93
Mauricio Villatoro <mauripr26@gmail.com>
2023-01-22 07:59:27 -0600

    :sparkles: Show zones

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,8 @@ use Illuminate\Support\Facades\Route;
 use \App\Http\Controllers\Api\V1\Auth;
 
 Route::prefix('v1')->group(function () {
+    //Get Zones route
+    Route::get('zones', [\App\Http\Controllers\Api\V1\ZoneController::class, 'index']);
     Route::middleware('auth:sanctum')->group(function () {
         //Profile routes
         Route::get('profile', [Auth\ProfileController::class, 'show']);
@@ -12,7 +14,7 @@ Route::prefix('v1')->group(function () {
         //Vehicle routes
         Route::apiResource('vehicles', \App\Http\Controllers\Api\V1\VehicleController::class);
     });
-    
+
     Route::prefix('auth')->group(function () {
         Route::post('register', Auth\RegisterController::class);
         Route::post('login', Auth\LoginController::class);
